001d4472d9553c22fd3b8d872f663e230b53abe7
Muhamad Bayu <muhamad.bayu41@gmail.com>
2022-01-10 12:13:41 +0700

    update on ejs course

--- 22. EJS/todolist-v1/app.js
+++ 22. EJS/todolist-v1/app.js
@@ -1,21 +1,29 @@
 //jshint esversion:9
 const express = require('express');
 const bodyParser = require('body-parser');
+let ejs = require('ejs');
+
+// let people = ['geddy', 'neil', 'alex'];
+// let html = ejs.render('<%= people.join(", "); %>', {people: people});
 
 const app = new express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
 
 app.get("/",function (req,res) {
+
   var date = new Date();
   var today = date.getDay();
-  if (today==6||today==0) {
-      res.write("<h1>Yeay its weekend!</h1>");
-      res.write("<h3>It's time to play</h3>");
-  }
-  else {
-    res.sendFile(__dirname+"/index.html");
-  }
+  var todayName=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
 
+  // if (today==6||today==0) {
+  //   todayName = "weekend";
+  // }
+  // else {
+  //   todayName = "weekday";
+  // }
+
+res.render("list",{kindOfDay:todayName[today]});
 
 });
 
